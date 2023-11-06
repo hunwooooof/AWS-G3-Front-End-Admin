a2ed9212fe653468d702c59954fe26d38ed396bb
Wanyu Lee <wanyulee1998@gmail.com>
2023-11-06 15:54:40 +0800

    feat: Complete static field and preview of coupon

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import App from './App';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
+import Coupon from './pages/Coupon';
+import Login from './pages/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -10,8 +10,8 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />}>
-        <Route index element={<Home />} />
-        <Route path='profile' element={<Profile />} />
+        <Route index element={<Login />} />
+        <Route path='coupon' element={<Coupon />} />
         <Route path='*' element={<Navigate to='/' replace />} />
       </Route>
     </Routes>
---
+++ src/pages/Coupon/index.jsx
@@ -0,0 +1,181 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import discountImage from './discount.webp';
+import freeFreightImage from './free-freight-fee.webp';
+
+const Wrapper = styled.div`
+  height: 100%;
+  padding: 80px 200px 0px 550px;
+`;
+
+const Navigation = styled.div`
+  display: flex;
+  gap: 60px;
+  font-size: 30px;
+  border-bottom: 1px solid #bbb;
+  padding-bottom: 10px;
+`;
+
+const NavItem = styled.div`
+  padding: 10px;
+  border-radius: 8px;
+  &:hover {
+    cursor: pointer;
+    background-color: #dbdbdb;
+  }
+`;
+
+const Fields = styled.div`
+  padding: 40px 0 40px 40px;
+`;
+
+const Field = styled.div`
+  display: flex;
+  margin: 30px;
+  height: 30px;
+  font-size: 20px;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  width: 170px;
+  letter-spacing: 1.5px;
+  color: #575757;
+`;
+
+const Input = styled.input`
+  width: 330px;
+  height: 40px;
+  padding: 0 5px;
+  font-size: 20px;
+`;
+
+const Radio = styled.input`
+  width: 20px;
+  height: 20px;
+  margin: 10px;
+`;
+
+const Preview = styled.div`
+  letter-spacing: 1.5px;
+  color: #242424;
+  font-size: 24px;
+  margin: 20px 0;
+  padding: 20px;
+  border-bottom: 1px solid #bbb;
+`;
+
+const Item = styled.div`
+  margin: 0 auto;
+  width: 90%;
+  display: flex;
+  gap: 10px;
+  background-color: white;
+  box-shadow: 2px 2px 3px #cbcbcb;
+`;
+
+const Img = styled.img`
+  width: 30%;
+`;
+
+const ItemDetail = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const ItemInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ItemInfoName = styled.div`
+  letter-spacing: 1.2px;
+  font-weight: bold;
+  font-size: 22px;
+`;
+
+const GetButton = styled.button`
+  white-space: nowrap;
+  background-color: #3f3a3a;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  font-size: 20px;
+`;
+
+const ExpireDate = styled.div`
+  font-size: 18px;
+  color: #504d4d;
+`;
+
+const fields = [
+  { name: '活動名稱', type: 'text', id: 'title', placeholder: '顯示於官網作為優惠券名稱' },
+  { name: '發放數量', type: 'number', id: 'amount' },
+  { name: '折扣數', type: 'number', id: 'discount' },
+  { name: '開始日期', type: 'date', id: 'start_date' },
+  { name: '截止日期', type: 'date', id: 'expiry_date' },
+];
+
+function Coupon() {
+  const [newCoupon, setNewCoupon] = useState({
+    type: '折扣',
+    title: '',
+    discount: 0,
+    start_date: '',
+    expiry_date: '',
+    amount: 0,
+  });
+
+  const handleChangeInput = (e) => {
+    const id = e.target.id;
+    newCoupon[id] = e.target.value;
+    setNewCoupon({ ...newCoupon });
+  };
+
+  return (
+    <Wrapper>
+      <Navigation>
+        <NavItem>新增優惠券</NavItem>
+        <NavItem>查看所有優惠券</NavItem>
+      </Navigation>
+      <Fields>
+        {fields.map((field) => (
+          <Field key={field.id}>
+            <Label>{field.name}</Label>
+            <Input
+              type={field.type}
+              id={field.id}
+              value={newCoupon[field.id]}
+              placeholder={field?.placeholder}
+              onChange={handleChangeInput}
+            />
+          </Field>
+        ))}
+        <Field>
+          <Label>折扣類型</Label>
+          <Radio id='type' type='radio' name='type' value='折扣' onChange={handleChangeInput} />
+          折扣
+          <Radio id='type' type='radio' name='type' value='免運' onChange={handleChangeInput} />
+          免運
+        </Field>
+      </Fields>
+      <Preview>預覽畫面</Preview>
+      <Item>
+        <Img src={newCoupon.type === '折扣' ? discountImage : freeFreightImage} />
+        <ItemDetail>
+          <ItemInfo>
+            <ItemInfoName>{newCoupon.title}</ItemInfoName>
+            <GetButton>領取</GetButton>
+          </ItemInfo>
+          <ExpireDate>有效期限：{newCoupon.expiry_date}</ExpireDate>
+        </ItemDetail>
+      </Item>
+    </Wrapper>
+  );
+}
+
+export default Coupon;
